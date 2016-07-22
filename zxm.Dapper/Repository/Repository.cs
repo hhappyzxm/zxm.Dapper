8474a7f3f0ad7de3247f877a6a6864680b0dde04
zxm <hhappyzxm@163.com>
2016-07-22 08:16:42 +0800

    0.0.2 Add new base repository without generic

--- zxm.Dapper/Repository/Repository.cs
+++ zxm.Dapper/Repository/Repository.cs
@@ -7,6 +7,26 @@ namespace zxm.Dapper
     /// <summary>
     /// Repository
     /// </summary>
+    public class Repository : IRepository
+    {
+        /// <summary>
+        /// Db Connection
+        /// </summary>
+        public IDbConnection Connection { get; }
+
+        /// <summary>
+        /// Constructor of Repository
+        /// </summary>
+        /// <param name="connection"></param>
+        public Repository(IDbConnection connection)
+        {
+            Connection = connection;
+        }
+    }
+
+    /// <summary>
+    /// Repository
+    /// </summary>
     /// <typeparam name="TEntity"></typeparam>
     public class Repository<TEntity> : DapperRepository<TEntity>, IRepository<TEntity>
         where TEntity : class
