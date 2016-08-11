97ad6837f8f1d63337756071591553c6b37c72a5
zxm <hhappyzxm@163.com>
2016-08-11 12:50:53 +0800

    Add zxm.Dapper.Extensions.AspNetCore

---
+++ src/zxm.Dapper.Extenstions.AspNetCore/DapperServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace zxm.Dapper.Extenstions.AspNetCore
+{
+    public static class DapperServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMailKit(this IServiceCollection services, string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException(connectionString);
+            }
+
+            return AddMailKit(services, () => new DbContext(new SqlConnection(connectionString)));
+        }
+
+        public static IServiceCollection AddMailKit(this IServiceCollection services, Func<IDbContext> dbContextBuilder)
+        {
+            if (dbContextBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(dbContextBuilder));
+            }
+            
+            services.AddSingleton(provider => dbContextBuilder());
+
+            return services;
+        }
+    }
+}
