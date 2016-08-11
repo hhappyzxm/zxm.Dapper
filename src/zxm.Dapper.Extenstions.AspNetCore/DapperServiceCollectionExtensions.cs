c6d48e9a48104d32bfb657fde06f4f44435bc481
zxm <hhappyzxm@163.com>
2016-08-11 13:21:41 +0800

    Change DI type to scoped

--- src/zxm.Dapper.Extenstions.AspNetCore/DapperServiceCollectionExtensions.cs
+++ src/zxm.Dapper.Extenstions.AspNetCore/DapperServiceCollectionExtensions.cs
@@ -6,24 +6,24 @@ namespace zxm.Dapper.Extenstions.AspNetCore
 {
     public static class DapperServiceCollectionExtensions
     {
-        public static IServiceCollection AddMailKit(this IServiceCollection services, string connectionString)
+        public static IServiceCollection AddDapper(this IServiceCollection services, string connectionString)
         {
             if (string.IsNullOrEmpty(connectionString))
             {
                 throw new ArgumentNullException(connectionString);
             }
 
-            return AddMailKit(services, () => new DbContext(new SqlConnection(connectionString)));
+            return AddDapper(services, () => new DbContext(new SqlConnection(connectionString)));
         }
 
-        public static IServiceCollection AddMailKit(this IServiceCollection services, Func<IDbContext> dbContextBuilder)
+        public static IServiceCollection AddDapper(this IServiceCollection services, Func<IDbContext> dbContextBuilder)
         {
             if (dbContextBuilder == null)
             {
                 throw new ArgumentNullException(nameof(dbContextBuilder));
             }
             
-            services.AddSingleton(provider => dbContextBuilder());
+            services.AddScoped(provider => dbContextBuilder());
 
             return services;
         }
